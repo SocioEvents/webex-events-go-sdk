d2e69913b1cf8687bb3d672a7fb922dcc02a04f1
Mehmet Yıldız <yildiz@cisco.com>
2023-12-12 14:21:04 +0300

    tests for ratelimiter details

---
+++ rate_limiter_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	_ "github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func Test_fillRateLimiter(t *testing.T) {
+	var mockResponse = http.Response{
+		Header: http.Header{
+			"Content-Type":           []string{"application/json"},
+			"X-Daily-Call-Limit":     []string{"10/200"},
+			"X-Secondly-Call-Limit":  []string{"5/50"},
+			"X-Daily-Retry-After":    []string{"3"},
+			"X-Secondly-Retry-After": []string{"5"},
+		},
+	}
+
+	var rateLimiter = RateLimiter{}
+	fillRateLimiter(&mockResponse, &rateLimiter)
+	assert.Equal(t, rateLimiter.DailyBasedCostThreshold, 200)
+	assert.Equal(t, rateLimiter.UsedDailyBasedCost, 10)
+	assert.Equal(t, rateLimiter.SecondBasedCostThreshold, 50)
+	assert.Equal(t, rateLimiter.UsedSecondBasedCost, 5)
+	assert.Equal(t, rateLimiter.DailyRetryAfter, 3)
+	assert.Equal(t, rateLimiter.SecondlyRetryAfter, 5)
+}
